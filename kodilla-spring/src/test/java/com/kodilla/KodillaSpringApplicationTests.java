ba8078afc02ca92e818f87ccfc8a8afdec576d7a
Mateusz Dąbrowski <mat.dabrowski@yahoo.com>
2021-01-23 12:47:04 +0100

    Module 14.2

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -2,6 +2,7 @@ package com.kodilla;
 
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Square;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +21,7 @@ class KodillaSpringApplicationTests {
 	void testCircleLoadedIntoContainer() {
 		//Given
 		ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Circle.class);
+		Shape shape = (Shape)context.getBean("circle");
 
 		//when
 		String name = shape.getShapeName();
@@ -33,7 +34,7 @@ class KodillaSpringApplicationTests {
 	void testTriangleLoadedIntoContainer() {
 		//Given
 		ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Triangle.class);
+		Shape shape = (Shape)context.getBean("triangle");
 
 		//when
 		String name = shape.getShapeName();
@@ -41,4 +42,31 @@ class KodillaSpringApplicationTests {
 		//then
 		assertEquals("This is a triangle.", name);
 	}
+	@Test
+	void testSquareLoadedIntoContainer() {
+		//Given
+		ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+		Shape shape = (Shape)context.getBean("createSquare");
+
+		//when
+		String name = shape.getShapeName();
+
+		//then
+		assertEquals("This is a square.", name);
+
+	}
+
+	@Test
+	void testShapeLoadedIntoContainer(){
+		//given
+		ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+		Shape shape	= (Shape)context.getBean("chosenShape");
+
+		//when
+		String name = shape.getShapeName();
+
+		//then
+		System.out.println("Chosen shape says: " + name);
+	}
+
 }
